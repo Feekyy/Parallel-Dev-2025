6bc4b0bff9b99a753d1163ea2f3b372962904b0d
Vitkolczi Dániel <90444730+Feekyy@users.noreply.github.com>
2024-03-05 17:16:59 +0100

    Add files via upload

---
+++ kernel/kernel_loader.h
@@ -0,0 +1,21 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include <CL/cl.h>
+
+typedef struct Program
+{
+    cl_device_id did;
+    cl_uint platforms;
+    cl_uint devices;
+	cl_platform_id pid;
+    cl_context context;
+    cl_program program;
+    int err;
+} Program;
+
+char* createKernel();
+
+void buildProgram(struct Program* prog, char* kernel_code);
+
+#endif
