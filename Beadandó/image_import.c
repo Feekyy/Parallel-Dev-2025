b33d1395949d19906e280297f63cd045eb7c2ecc
Vitkolczi Dániel <90444730+Feekyy@users.noreply.github.com>
2025-03-25 06:41:04 +0100

    Add files via upload
    Updating with the local files

--- Beadandó/image_import.c
+++ Beadandó/image_import.c
@@ -12,15 +12,14 @@ void import(Image* ima, char filename[20])
     int height;
     int width;
 
-    unsigned char* rgb_data;
+    int* rgb_data;
     char path[] = "images/";
     strcat(path, filename);
-    printf("\nPath: %s\n", path);
     FILE *file = fopen(path, "rb");
 
     if (file == NULL)
     {
-        printf("File is empty!");
+        printf("File does not exist!\n");
         return;
     }
 
@@ -29,20 +28,37 @@ void import(Image* ima, char filename[20])
 
     fclose(file);
 
-    FILE *out = fopen("images/colors_horse.ppm", "wb");
+    FILE *out = fopen("images/greymap.pgm", "wb+");
 
-    fprintf(out, "P3\n");
+    fprintf(out, "P2\n");
     fprintf(out, "%d %d\n", width, height);
     fprintf(out, "255\n");
 
-    for (int i = 0; i < height*width; ++i) 
+    int k = 3 * width;
+    ima->picMatrix = (unsigned char**)malloc(height * sizeof(unsigned char*));
+
+    for (int i = 0; i < height; ++i) 
     {
-            fprintf(out, "%d %d %d ", rgb_data[3 * i], rgb_data[3 * i + 1], rgb_data[3 * i + 2]);
+        ima->picMatrix[i] = (int *)malloc(width * sizeof(int));
+        for (int k = 0; k < width; k++)
+        {
+
+            ima->picMatrix[i][k] = (rgb_data[(3 * i * width) + k] + rgb_data[(3 * i * width + 1) + k] + rgb_data[(3 * i * width + 2) + k])/3;
+            printf("Matrixnumb: %d , i:%d, k:%d, 1:%d, 2:%d, 3:%d\n", ima->picMatrix[i][k], i, k, rgb_data[(3 * i * width) + k], rgb_data[(3 * i * width + 1)], rgb_data[(3 * i * width + 2) + k]);
+
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < height*width; ++i)
+    {
+        fprintf(out, "%d ", (rgb_data[3 * i] + rgb_data[3 * i + 1] + rgb_data[3 * i + 2])/3);
     }
 
     ima->fp = out;
     ima->height = height;
     ima->width = width;
+    ima->maxCol = 255;
     fclose(out);
     stbi_image_free(rgb_data);
 
--- Beadandó/image_import.h
+++ Beadandó/image_import.h
@@ -6,6 +6,8 @@ typedef struct Image
     FILE *fp;
     int height;
     int width;
+    int maxCol;
+    int** picMatrix;
 } Image;
 
 void import(Image* ima, char filename[20]);
